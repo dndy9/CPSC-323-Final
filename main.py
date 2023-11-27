4c468978ba205d29a7737806e4e2b563d309dea9
ChrisA <christopherascencio00@gmail.com>
2023-11-27 02:20:39 -0800

    Added old code to main

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+file_path = 'finalv1.txt'
+try:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print(f"The file '{file_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
 with open("finalv1.txt", "r") as input_file:
     lines = input_file.readlines()
 
