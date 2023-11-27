da51156df6e226fdc474e41a157d9b8f64d7a7e7
ChrisA <christopherascencio00@gmail.com>
2023-11-27 02:17:51 -0800

    Updated main, makes text file
    added new logic to main, deleted what was in there before

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-file_path = 'finalv1.txt'
+with open("finalv1.txt", "r") as input_file:
+    lines = input_file.readlines()
 
-try:
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-except FileNotFoundError:
-    print(f"The file '{file_path}' was not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+modified_lines = []
 
+for line in lines:
+    # remove comments
+    line = line.split("(*")[0].strip()
 
+    # remove extra spaces
+    line = " ".join(line.split())
 
+    # skip blank lines
+    if line != "":
+        modified_lines.append(line)
+
+with open("final23.txt", "w") as output_file:
+    output_file.write("\n".join(modified_lines))
