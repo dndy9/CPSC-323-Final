b52fabaa45b38d7cc4247d0e83d7868b3a691098
ColdShaker <michaelball357@gmail.com>
2023-12-05 00:11:58 -0800

    adding '0'

--- pt2.py
+++ pt2.py
@@ -1,15 +1,15 @@
 from parsing_table import *
 from read_final23 import get_final23
-column_names = ['a','b','c','d','w','f','+','-','(',')','*','/','1','2','3','4','5','6','7','8','9','$',';','program','var','begin','integer','write','value=']
+column_names = ['a','b','c','d','w','f','+','-','(',')','*','/','0','1','2','3','4','5','6','7','8','9','$',';','program','var','begin','integer','write','value=']
 row_names = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
 
 
 def t_response(row, column, table):
     try:
+        print("row index: ",row_names.index(row))
+        print("column index: ",column)
         row_index = row_names.index(row)
         column_index = column_names.index(column)
-        print("row index: ",row_index)
-        print("column index: ",column_index)
         return table[row_index][column_index]
     except ValueError:
         print(f"Error: Row '{row}' or Column '{column}' not found in the table.")
